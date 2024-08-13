25e26417538da97844ad7ef1750e3dae823c394a
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-13 19:20:57 +0300

    fix

--- ewm-stats/stats-model/src/main/java/ru/practicum/model/HitDto.java
+++ ewm-stats/stats-model/src/main/java/ru/practicum/model/HitDto.java
@@ -1,13 +1,8 @@
 package ru.practicum.model;
 
 
-
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -23,16 +18,14 @@ public class HitDto {
     @NotNull
     private String app;
 
-    @NotEmpty
-    @Pattern(regexp = "^/.*")
+    @NotNull
     private String uri;
 
-    @NotEmpty
-    @Pattern(regexp = "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)(\\.(?!$)|$)){4}$")
+    @NotNull
     private String ip;
 
-    @PastOrPresent
-    @JsonProperty("timestamp")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime requestDateTime;
 }
