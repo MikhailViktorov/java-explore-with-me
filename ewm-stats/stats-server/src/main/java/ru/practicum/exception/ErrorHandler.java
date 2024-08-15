de727a67695c07240bb9b13f6a6118173b2ec35b
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-15 13:06:55 +0300

    fix

--- ewm-stats/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,20 +1,20 @@
 package ru.practicum.exception;
 
-import jakarta.validation.ValidationException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
+
 @RestControllerAdvice
-@Slf4j
 public class ErrorHandler {
 
+
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ValidationException.class)
-    public String handleValidationExceptions(ValidationException ex) {
-        log.debug("Получен код ошибки 409 bad request");
-        return ex.getMessage();
+    public Map<String, String> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        return Map.of("error", e.getMessage());
     }
 }
