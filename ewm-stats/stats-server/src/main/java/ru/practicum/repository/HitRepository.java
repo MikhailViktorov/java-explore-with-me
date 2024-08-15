9a89f4cc0b4a8f3a0a1c7cab02419c46bab8b203
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-15 15:28:17 +0300

    fix

--- ewm-stats/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -12,21 +12,21 @@ import java.util.List;
 @Repository
 public interface HitRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT h.app, h.uri, COUNT (h.ip)"
+    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT (h.ip))"
             + "FROM EndpointHit AS h "
             + "WHERE h.timestamp BETWEEN ?1 and ?2 "
             + "GROUP BY h.uri, h.app "
             + "ORDER BY COUNT (h.ip) DESC ")
     List<ViewStats> findHits(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT h.app, h.uri, count(h.ip) "
+    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, count(h.ip)) "
             + "FROM EndpointHit AS h "
             + "WHERE h.timestamp BETWEEN ?1 AND ?2 "
             + "GROUP BY h.uri, h.app "
             + "ORDER BY COUNT(h.ip) DESC ")
     List<ViewStats> findDistinctHits(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT h.app, h.uri, count(distinct h.ip) "
+    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) "
             + "FROM EndpointHit h "
             + "WHERE h.timestamp BETWEEN ?1 AND ?2 "
             + "AND h.uri IN ?3 "
@@ -34,11 +34,13 @@ public interface HitRepository extends JpaRepository<EndpointHit, Long> {
             + "ORDER BY COUNT (h.ip) DESC ")
     List<ViewStats> findDistinctHitsByUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT h.app, h.uri, COUNT (h.ip)"
+    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT (h.ip))"
             + "FROM EndpointHit h "
             + "WHERE h.timestamp BETWEEN ?1 AND ?2 "
             + "AND h.uri in ?3 "
             + "GROUP BY h.uri, h.app "
             + "ORDER BY COUNT (h.ip) DESC ")
     List<ViewStats> findHitsByUris(LocalDateTime start, LocalDateTime end, List<String> uris);
+
+
 }
