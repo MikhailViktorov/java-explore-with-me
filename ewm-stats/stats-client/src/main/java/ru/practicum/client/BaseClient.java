c8a5d714d01d385ac043284a2d8f1697e583986e
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-15 15:09:15 +0300

    fix

--- ewm-stats/stats-client/src/main/java/ru/practicum/client/BaseClient.java
+++ ewm-stats/stats-client/src/main/java/ru/practicum/client/BaseClient.java
@@ -1,57 +1,40 @@
 package ru.practicum.client;
 
+import jakarta.annotation.Nullable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
-import org.springframework.lang.Nullable;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
 
+@RequiredArgsConstructor
 public class BaseClient {
-    protected final RestTemplate rest;
+    protected final RestTemplate restTemplate;
 
-    public BaseClient(RestTemplate rest) {
-        this.rest = rest;
+    protected ResponseEntity<Object> get(String uri, @Nullable Map<String, Object> parameters) {
+        return sendRequest(uri, HttpMethod.GET, null, parameters);
     }
 
-    private static ResponseEntity<Object> prepareResponse(ResponseEntity<Object> response) {
-        if (response.getStatusCode().is2xxSuccessful()) {
-            return response;
+    protected <T> ResponseEntity<Object> post(String uri, @Nullable T body,
+                                              @jakarta.annotation.Nullable Map<String, Object> parameters) {
+        return sendRequest(uri, HttpMethod.POST, body, parameters);
+    }
+
+    private <T> ResponseEntity<Object> sendRequest(String uri, HttpMethod method,
+                                                   @Nullable T body,
+                                                   @Nullable Map<String, Object> parameters) {
+
+        HttpEntity<T> request = new HttpEntity<>(body, defaultHeaders());
+
+        ResponseEntity<Object> response;
+
+        if (parameters == null) {
+            response = restTemplate.exchange(uri, method, request, Object.class);
+        } else {
+            response = restTemplate.exchange(uri, method, request, Object.class, parameters);
         }
-
-        ResponseEntity.BodyBuilder responseBuilder = ResponseEntity.status(response.getStatusCode());
-
-        if (response.hasBody()) {
-            return responseBuilder.body(response.getBody());
-        }
-
-        return responseBuilder.build();
-    }
-
-    protected <T> ResponseEntity<Object> post(String path, T body) {
-        return makeAndSendRequest(HttpMethod.POST, path, null, body);
-    }
-
-    protected ResponseEntity<Object> get(String path, @Nullable Map<String, Object> parameters) {
-        return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
-    }
-
-    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path,
-                                                          @Nullable Map<String, Object> parameters,
-                                                          @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
-        ResponseEntity<Object> serverResponse;
-        try {
-            if (parameters != null) {
-                serverResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
-            } else {
-                serverResponse = rest.exchange(path, method, requestEntity, Object.class);
-            }
-        } catch (HttpStatusCodeException e) {
-            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
-        }
-        return prepareResponse(serverResponse);
+        return response;
     }
 
     private HttpHeaders defaultHeaders() {
