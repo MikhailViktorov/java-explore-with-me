daf9351434d693917e50a9e03e8106809c280a4b
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-15 12:33:22 +0300

    fix

--- ewm-stats/stats-client/src/main/java/ru/practicum/client/BaseClient.java
+++ ewm-stats/stats-client/src/main/java/ru/practicum/client/BaseClient.java
@@ -9,46 +9,13 @@ import java.util.List;
 import java.util.Map;
 
 public class BaseClient {
-
     protected final RestTemplate rest;
 
     public BaseClient(RestTemplate rest) {
         this.rest = rest;
     }
 
-    protected <T> ResponseEntity<Object> post(T body) {
-        return makeAndSendRequest(HttpMethod.POST, "/hit", null, body);
-    }
-
-    protected ResponseEntity<Object> get(String path, Map<String, Object> parameters) {
-        return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
-    }
-
-    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path,
-                                                          @Nullable Map<String, Object> parameters, @Nullable T body) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
-
-        ResponseEntity<Object> response;
-        try {
-            if (parameters != null) {
-                response = rest.exchange(path, method, requestEntity, Object.class, parameters);
-            } else {
-                response = rest.exchange(path, method, requestEntity, Object.class);
-            }
-        } catch (HttpStatusCodeException e) {
-            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
-        }
-        return prepareGatewayResponse(response);
-    }
-
-    private HttpHeaders defaultHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        return headers;
-    }
-
-    private static ResponseEntity<Object> prepareGatewayResponse(ResponseEntity<Object> response) {
+    private static ResponseEntity<Object> prepareResponse(ResponseEntity<Object> response) {
         if (response.getStatusCode().is2xxSuccessful()) {
             return response;
         }
@@ -60,5 +27,37 @@ public class BaseClient {
         }
 
         return responseBuilder.build();
+    }
+
+    protected <T> ResponseEntity<Object> post(String path, T body) {
+        return makeAndSendRequest(HttpMethod.POST, path, null, body);
+    }
+
+    protected ResponseEntity<Object> get(String path, @Nullable Map<String, Object> parameters) {
+        return makeAndSendRequest(HttpMethod.GET, path, parameters, null);
+    }
+
+    private <T> ResponseEntity<Object> makeAndSendRequest(HttpMethod method, String path,
+                                                          @Nullable Map<String, Object> parameters,
+                                                          @Nullable T body) {
+        HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders());
+        ResponseEntity<Object> serverResponse;
+        try {
+            if (parameters != null) {
+                serverResponse = rest.exchange(path, method, requestEntity, Object.class, parameters);
+            } else {
+                serverResponse = rest.exchange(path, method, requestEntity, Object.class);
+            }
+        } catch (HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsByteArray());
+        }
+        return prepareResponse(serverResponse);
+    }
+
+    private HttpHeaders defaultHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        return headers;
     }
 }
--- ewm-stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ ewm-stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -1,72 +1,42 @@
 package ru.practicum.client;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.model.EndpointHitDto;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
-@Component
+@Service
 public class StatsClient extends BaseClient {
+    private static final String API_PRFX_STATS = "/stats";
+    private static final String API_PRFX_HIT = "/hit";
 
-    private static final String MAIN_SERVICE_NAME = "ewm-main-service";
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-    public StatsClient(RestTemplateBuilder builder) {
-        super(builder
-                .uriTemplateHandler(new DefaultUriBuilderFactory("http://stats-server:9090"))
+    @Autowired
+    public StatsClient(@Value("${stats-service.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
                 .build());
     }
 
-    public ResponseEntity<Object> hit(String uri, String ip,
-                                      LocalDateTime requestDateTime) {
-        EndpointHitDto endpointHitDto = new EndpointHitDto(MAIN_SERVICE_NAME, uri, ip, requestDateTime);
-        return post(endpointHitDto);
-    }
-
-    public ResponseEntity<Object> getStats(LocalDateTime startDateTime, LocalDateTime endDateTime) {
-        Map<String, Object> parameters = Map.of("start", startDateTime.format(formatter), "end",
-                endDateTime.format(formatter));
-        return get("/stats?start={start}&end={end}", parameters);
-    }
-
-    public ResponseEntity<Object> getStats(LocalDateTime startDateTime, LocalDateTime endDateTime,
-                                           List<String> uris) {
+    public ResponseEntity<Object> getStats(String start, String end, List<String> uris, Boolean unique) {
+        String path = "?start={start}&end={end}&uris={uris}&unique={unique}";
         Map<String, Object> parameters = Map.of(
-                "start", startDateTime.format(formatter),
-                "end", endDateTime.format(formatter),
-                "uris", uris
-        );
-        return get("/stats?start={start}&end={end}&uris={uris}", parameters);
-    }
-
-    public ResponseEntity<Object> getStats(LocalDateTime startDateTime, LocalDateTime endDateTime,
-                                           String[] uris, boolean unique) {
-        Map<String, Object> parameters = Map.of(
-                "start", startDateTime.format(formatter),
-                "end", endDateTime.format(formatter),
+                "start", start,
+                "end", end,
                 "uris", uris,
                 "unique", unique
         );
-        return get("/stats?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
+        return get(API_PRFX_STATS + path, parameters);
     }
 
-    public ResponseEntity<Object> getStats(String startDateTime, String endDateTime,
-                                           boolean unique) {
-        Map<String, Object> parameters = Map.of(
-                "start", startDateTime,
-                "end", endDateTime,
-                "unique", unique
-        );
-        return get("/stats?start={start}&end={end}&unique={unique}", parameters);
+    public ResponseEntity<Object> createHit(EndpointHitDto dto) {
+        return post(API_PRFX_HIT, dto);
     }
-
 }
