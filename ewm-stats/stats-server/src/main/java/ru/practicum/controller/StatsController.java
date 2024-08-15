a1153dc3cc333e23f627034a858617ed51f6b226
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-15 16:56:11 +0300

    fix

--- ewm-stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/controller/StatsController.java
@@ -1,13 +1,11 @@
 package ru.practicum.controller;
 
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.model.EndpointHitDto;
 import ru.practicum.model.ViewStatsDto;
