6cb95b9d2e6332d873f52cbf19c1f8ec1b78bb25
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-15 15:33:26 +0300

    fix

--- ewm-stats/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/repository/HitRepository.java
@@ -12,21 +12,21 @@ import java.util.List;
 @Repository
 public interface HitRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT (h.ip))"
+    @Query("SELECT h.app, h.uri, COUNT (h.ip) AS hits "
             + "FROM EndpointHit AS h "
             + "WHERE h.timestamp BETWEEN ?1 and ?2 "
             + "GROUP BY h.uri, h.app "
             + "ORDER BY COUNT (h.ip) DESC ")
     List<ViewStats> findHits(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, count(h.ip)) "
+    @Query("SELECT h.app, h.uri, count(h.ip) AS hits "
             + "FROM EndpointHit AS h "
             + "WHERE h.timestamp BETWEEN ?1 AND ?2 "
             + "GROUP BY h.uri, h.app "
             + "ORDER BY COUNT(h.ip) DESC ")
     List<ViewStats> findDistinctHits(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, count(distinct h.ip)) "
+    @Query("SELECT h.app, h.uri, count(distinct h.ip) AS hits "
             + "FROM EndpointHit h "
             + "WHERE h.timestamp BETWEEN ?1 AND ?2 "
             + "AND h.uri IN ?3 "
@@ -34,7 +34,7 @@ public interface HitRepository extends JpaRepository<EndpointHit, Long> {
             + "ORDER BY COUNT (h.ip) DESC ")
     List<ViewStats> findDistinctHitsByUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.model.ViewStats(h.app, h.uri, COUNT (h.ip))"
+    @Query("SELECT h.app, h.uri, COUNT (h.ip) AS hits "
             + "FROM EndpointHit h "
             + "WHERE h.timestamp BETWEEN ?1 AND ?2 "
             + "AND h.uri in ?3 "
