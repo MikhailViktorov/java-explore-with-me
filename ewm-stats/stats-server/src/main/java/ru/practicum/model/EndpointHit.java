5bec331051cf76207126ebf6361cc420991a779a
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-15 14:53:18 +0300

    fix

--- ewm-stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/model/EndpointHit.java
@@ -17,12 +17,12 @@ public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "app")
+
     private String app;
-    @Column(name = "uri")
+
     private String uri;
-    @Column(name = "ip")
+
     private String ip;
-    @Column(name = "timestamp")
+
     private LocalDateTime timestamp;
 }
