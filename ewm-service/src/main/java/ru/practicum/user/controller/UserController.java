02206de35fed5152b867b9ae927de240fc206d00
Mikhail Viktorov <the-rayn@yandex.ru>
2024-08-19 12:34:25 +0300

    add user logic

--- ewm-service/src/main/java/ru/practicum/user/controller/UserController.java
+++ ewm-service/src/main/java/ru/practicum/user/controller/UserController.java
@@ -2,7 +2,10 @@ package ru.practicum.user.controller;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,21 +13,36 @@ import ru.practicum.user.dto.NewUserRequest;
 import ru.practicum.user.dto.UserDto;
 import ru.practicum.user.service.UserService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
     @PostMapping
     public ResponseEntity<UserDto> addUser(@RequestBody @Valid NewUserRequest newUserRequest) {
+        log.info("Create user: {}", newUserRequest);
         UserDto userDto = userService.addUser(newUserRequest);
         return new ResponseEntity<>(userDto, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{userId}")
     public ResponseEntity<Void> deleteUser(@PathVariable @Positive Long userId) {
+        log.info("Delete user with id  {}", userId);
         userService.deleteUser(userId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers(@RequestParam(defaultValue = "") List<Long> ids,
+                                                     @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                                                     @RequestParam(defaultValue = "10") @Positive int size) {
+        log.info("Admin request all users");
+        int page = from / size;
+        PageRequest pageRequest = PageRequest.of(page, size);
+        List<UserDto> users = userService.getAllUsers(ids, pageRequest);
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
--- ewm-service/src/main/java/ru/practicum/user/service/UserServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/user/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.practicum.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import ru.practicum.exception.NotFoundException;
 import ru.practicum.user.dto.NewUserRequest;
@@ -8,6 +9,10 @@ import ru.practicum.user.dto.UserDto;
 import ru.practicum.user.mapper.UserMapper;
 import ru.practicum.user.model.User;
 import ru.practicum.user.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
@@ -25,5 +30,18 @@ public class UserServiceImpl implements UserService{
         userRepository.findById(userId).orElseThrow(() ->
                 new NotFoundException("Пользователь с id " + userId + " не найден"));
         userRepository.deleteById(userId);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers(List<Long> ids, PageRequest pageRequest) {
+
+        List<User> users;
+
+        if (ids.isEmpty()) {
+            users = userRepository.findAll(pageRequest).stream().collect(Collectors.toList());
+        } else {
+            users = userRepository.findAllByIdIn(ids, pageRequest);
+        }
+        return users.stream().map(userMapper::userToUserDto).collect(Collectors.toList());
     }
 }
